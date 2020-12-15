e605dbb651d614b08e8b253f9d1463def545e39c
Li Daoyi <819574743@qq.com>
2020-12-15 19:44:06 +0800

    2020/12/15 19:40 修改代码适应实验报告

--- Lab6/test.c
+++ Lab6/test.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
-#define CLS(x, n) ((x) << (n)) | ((x) >> ((32) - (n)))
-int main(void){
-    unsigned int a=0xffffffff;
-    a += 0xffffffff;
-    // long b;printf("%d\n",sizeof(b));
-    printf("%x\n",sizeof(a));
-    printf("%x\n",a);printf("%x\n",CLS(a,30));
+int main(void)
+{
+    unsigned int after[5] = {0x4cb1c576,0xbb32db00,0x0ac73786,0xd0ee8fee ,0x0afd6e37};
+    unsigned int before[5] = {0x6fefa324 ,0xee16c477 ,0x9cb2aeb2 ,0x5c1afdcd ,0x2e9cd559};
+    unsigned int temp = 0;
+    int count = 0;
+    int i,j,k;
+    for(i=0;i<5;i++)
+    {
+        temp = before[i] ^ after[i];
+        for(j=0;j<32;j++)
+        {
+            k = temp % 2;
+            if(k == 1)
+            {
+                count++;
+            }
+            temp = temp / 2;
+        }
+    }
+    printf("%d\n",count);
 }
